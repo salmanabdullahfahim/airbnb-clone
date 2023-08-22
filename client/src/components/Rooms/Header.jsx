c184cd138eb6754fe7451e23337c7d151e6e4e05
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-22 21:31:06 +0600

    private route added in the room details page

--- client/src/components/Rooms/Header.jsx
+++ client/src/components/Rooms/Header.jsx
@@ -8,6 +8,13 @@ const Header = () => {
         title="Veluvana Bali - owl bamboo house"
         subtitle="Sidemen, Indonesia"
       />
+      <div className="w-full md:h-[60vh] overflow-hidden rounded-xl ">
+        <img
+          className="object-cover w-full "
+          src="https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg"
+          alt=""
+        />
+      </div>
     </>
   );
 };
--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../../components/Shared/Container";
 import Header from "../../components/Rooms/Header";
+import RoomInfo from "../../components/Rooms/RoomInfo";
 
 const RoomDetails = () => {
   return (
@@ -8,8 +9,11 @@ const RoomDetails = () => {
       <div className="max-w-screen-lg mx-auto">
         <div className="flex flex-col gap-6">
           <Header />
-          <div>Room Info</div>
-          <div>Calender</div>
+
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <RoomInfo />
+            <div>Calender</div>
+          </div>
         </div>
       </div>
     </Container>
