ca894b40e3f748dd32b20e0f89a7617279bdf865
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-22 17:29:28 +0600

    dynamically room card data showed

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -2,21 +2,32 @@ import React, { useState } from "react";
 import Container from "../Shared/Container";
 import Card from "./Card";
 import { useEffect } from "react";
+import Loader from "../Shared/Loader";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch("./rooms.json")
       .then((res) => res.json())
-      .then((data) => setRooms(data))
+      .then((data) => {
+        setRooms(data);
+        setLoading(false);
+      })
       .catch((err) => console.error(err));
   }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <Container>
       <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {rooms.map((room) => (
-          <Card />
+        {rooms.map((room, index) => (
+          <Card key={index} room={room} />
         ))}
       </div>
     </Container>
