fa6ca83a4e3a7b9b28ea496ef9511fc90fc87326
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-22 17:18:33 +0600

    rooms card added in the home page

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Shared/Container";
+import Card from "./Card";
+import { useEffect } from "react";
 
 const Rooms = () => {
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    fetch("./rooms.json")
+      .then((res) => res.json())
+      .then((data) => setRooms(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <Container>
-      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"></div>
+      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {rooms.map((room) => (
+          <Card />
+        ))}
+      </div>
     </Container>
   );
 };
