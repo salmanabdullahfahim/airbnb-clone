c2f679b10ba13b656615d8c322d1d98fd1364b3f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-10 19:55:52 +0600

    form all value grabbed

--- client/src/pages/Dashboard/AddRoom.jsx
+++ client/src/pages/Dashboard/AddRoom.jsx
@@ -5,7 +5,18 @@ const AddRoom = () => {
   //handle form submit
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("hello from form");
+    const location = event.target.location.value;
+    const category = event.target.category.value;
+    const title = event.target.title.value;
+    const price = event.target.price.value;
+    const bedrooms = event.target.bedrooms.value;
+    const bathrooms = event.target.bathrooms.value;
+    const description = event.target.description.value;
+    const total_guest = event.target.total_guest.value;
+    const from = dates.startDate;
+    const to = dates.endDate;
+
+    const image = event.target.image.files[0];
   };
   return (
     <div>
